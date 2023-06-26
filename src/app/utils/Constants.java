1b5619164d7bc7c0570348e8b7e08f7d968cfedb
Tanya <krajnikova2004@gmail.com>
2023-06-26 14:56:46 +0100

    Create constants: database access

---
+++ src/app/utils/Constants.java
@@ -0,0 +1,9 @@
+package app.utils;
+
+public class Constants {
+
+    public final static String DB_DRIVER = "jdbc:sqlite:";
+    public final static String DB_BASE_URL = "C:\\Users\\Tanya\\Hillel1.0\\Fruits-selfwork\\database\\";
+    public final static String DB_NAME = "fruitsdb.db";
+    public final static String TABLE_NAME = "fruits";
+}
