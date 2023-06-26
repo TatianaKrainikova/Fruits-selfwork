f4b63152158cb8d014138ac72b1512e942d4e02a
Tanya <krajnikova2004@gmail.com>
2023-06-26 14:51:27 +0100

    Create an object model

---
+++ src/app/models/Fruit.java
@@ -0,0 +1,12 @@
+package app.models;
+
+public class Fruit {
+
+    String name;
+    String description;
+
+    public Fruit(String name, String description) {
+        this.name = name;
+        this.description = description;
+    }
+}
